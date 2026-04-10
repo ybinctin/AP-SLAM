c22b8cb3f10d2dc12ada0e2f74cdeeeffe1784aa
baptiste-autret <shadowbaba.contact@gmail.com>
2026-04-10 16:17:07 +0200

    Feature : Modification du mot de passe

--- app/Controllers/ModificationMdp.php
+++ app/Controllers/ModificationMdp.php
@@ -17,6 +17,15 @@ class ModificationMdp extends BaseController
         $this->gsbLib = new Gsb_lib();
     }
 
+    /** Méthode par défaut */
+    public function index()
+    {
+        // Vérifie si l’utilisateur est connecté
+        if (!session()->get('isLoggedIn')) {
+            return redirect()->to('/');
+        }
+    }
+
     /**
      * Affiche l’écran de connexion
      */
@@ -56,21 +65,29 @@ class ModificationMdp extends BaseController
             return redirect()->back()->withInput()->with('validation', $this->validator);
         }
 
-        $login = $this->request->getPost(index: 'txtLogin');
-        $mdp = $this->request->getPost('pwdMdp');
+        $login = session()->get('login');
+        $mdp = $this->request->getPost('txtMdpActuel');
+
+        $nvMdp = $this->request->getPost('txtNvMdp');
+
 
         $utilisateur = $this->gsb_model->get_infos_utilisateur($login, $mdp);
-
+        
         if ($utilisateur) {
+
+            $this->gsb_model->update_mdp($login, $nvMdp);
+            
             session()->set([
                 'idUtilisateur' => $utilisateur['idutilisateur'],
+                'login' => $utilisateur['login'],
                 'nom' => $utilisateur['nom'],
                 'prenom' => $utilisateur['prenom'],
                 'role' => $utilisateur['idrole'],
                 'libellerole' => $utilisateur['libellerole'],
                 'isLoggedIn' => true
             ]);
-            return redirect()->to('/accueil');
+
+            return redirect()->to('/accueil')->withInput()->with('validation', 'Le mot de passe a été changé avec succès');
         }
 
         return redirect()->back()->withInput()->with('erreurs', "Votre mot de passe n'est pas assez fort ou est le même que le précédent. Veuillez réessayer.");
